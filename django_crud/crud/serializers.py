966207defd411985a02572df61b031a4dfd2dee6
Maksymilian Moskwytyn <79639894+maksblink@users.noreply.github.com>
2025-03-02 23:55:05 +0100

    readme

--- django_crud/crud/serializers.py
+++ django_crud/crud/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Measurement, Sensor, HydroponicSystem
-from django.utils.timezone import now
 
 
 class HydroponicSystemSerializer(serializers.ModelSerializer):
